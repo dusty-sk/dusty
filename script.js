1c285f21d630bb72a555ad496649735aedba21c0
dusty-sk <ondrej.remeselnik123@gmail.com>
2025-07-25 13:26:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,35 @@ window.addEventListener('load', revealOnScroll);
 
 
 
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "style-mobile.css";
+    document.head.appendChild(link);
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const overlay = document.createElement('div');
+    overlay.className = 'lightbox-overlay';
+
+    const lightbox = document.createElement('div');
+    lightbox.className = 'lightbox-image';
+
+    overlay.appendChild(lightbox);
+    document.body.appendChild(overlay);
+
+    document.querySelectorAll('.screenshot').forEach(screenshot => {
+        screenshot.addEventListener('click', () => {
+            const bg = window.getComputedStyle(screenshot).backgroundImage;
+            lightbox.style.backgroundImage = bg;
+            overlay.classList.add('active');
+        });
+    });
+
+    overlay.addEventListener('click', () => {
+        overlay.classList.remove('active');
+        lightbox.style.backgroundImage = '';
+    });
+});
